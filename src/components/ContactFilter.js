613588d22cb96b8bdb2b522b61c96ccd142d4933
awidiyadew <awidiya.dewa@gmail.com>
2018-03-07 01:36:00 +0700

    add functionality to contact filter and test

--- src/components/ContactFilter.js
+++ src/components/ContactFilter.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
 import { View, StyleSheet, TextInput } from 'react-native';
 import Button from './Button';
+import PropTypes from 'prop-types';
 
 export default class ContactFilter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      keyword: '',
+    };
+    this._onChangeKeyword = this._onChangeKeyword.bind(this);
+    this._onFilterContact = this._onFilterContact.bind(this);
+  }
+
+  _onChangeKeyword(keyword) {
+    this.setState({ keyword });
+  }
+
+  _onFilterContact() {
+    const { onFilterContact } = this.props;
+    onFilterContact(this.state.keyword);
+  }
+
   render() {
     return (
       <View
@@ -10,16 +29,22 @@ export default class ContactFilter extends Component {
       >
         <TextInput
           style={styles.filterInput}
+          value={this.state.keyword}
           placeholder='Search here'
+          onChangeText={this._onChangeKeyword}
         />
         <Button
-          onPress={() => {}}
+          onPress={this._onFilterContact}
           value={'Search'}
         />
       </View>
     );
   }
 }
+
+ContactFilter.propTypes = {
+  onFilterContact: PropTypes.func.isRequired
+};
 
 const styles = StyleSheet.create({
   container: {
